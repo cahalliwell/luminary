d940a11ba37159147c61a61dc560b3199450a092
cahalliwell <cahalliwell@gmail.com>
2025-03-04 11:08:44 +0000

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Luminary() {
+    return (
+        <div>
+            <h1>Welcome to Luminary</h1>
+            <p>Exclusive masterclasses in breathtaking locations.</p>
+        </div>
+    );
+}
+
+export default Luminary;

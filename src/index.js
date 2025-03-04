08e9aaf078615db3563c93f103a350334128780f
cahalliwell <cahalliwell@gmail.com>
2025-03-04 11:15:56 +0000

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Luminary from "./App";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Luminary />
+  </React.StrictMode>
+);
